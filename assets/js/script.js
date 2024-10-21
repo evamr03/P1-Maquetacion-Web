57ffec13cf4b9e882b113fbc309df2e4a12c2190
evamr03 <evamendozareija@gmail.com>
2024-10-21 13:31:46 +0200

    Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+// window scroll
+$(window).on("scroll", function() {
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop >= 100){
+        $('body').addClass('fixed-header');
+    } else {
+        $('body').removeClass('fixed-header')
+    }
+});
+
+// Document Ready
+$(document).ready(function() {
+
+    // Typing animation
+    new Typed('#type-it', {
+        strings: ['creativos', 'simples', 'atrevidos'],
+        typeSpeed: 100,
+        loop: true
+      });
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 2,
+        margin:30,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            900:{
+                items:2
+            },
+        }
+    });
+
+    // One Page Scroll
+    $.scrollIt({
+        easing: 'linear',      // the easing function for animation
+        topOffset: -70           // offste (in px) for fixed top navigation
+      });
+});
