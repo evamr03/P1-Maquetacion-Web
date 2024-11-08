6611156306e9b6d3ccfb278305e9af87f7edcad7
evamr03 <evamendozareija@gmail.com>
2024-11-08 01:57:10 +0100

    Detalles enlaces y css

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,11 @@ $(document).ready(function() {
       });
 });
 
+// Portfolio
+ScrollReveal().reveal(".grid-wrapper > div", {
+    delay: 200,
+    distance: "50px",
+    interval: 100,
+    origin: "bottom",
+    scale: 0.9,
+});
--- assets/js/scriptportfolio.js
+++
@@ -1,8 +0,0 @@
-// Portfolio
-ScrollReveal().reveal(".grid-wrapper > div", {
-    delay: 200,
-    distance: "50px",
-    interval: 100,
-    origin: "bottom",
-    scale: 0.9,
-});
